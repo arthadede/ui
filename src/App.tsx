092cc9d50c6298b295225d5655c2bfd2fe9f2928
arthadede <arthadede@gmail.com>
2025-12-03 21:40:32 +0800

    FEAT: Setup REUI

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import StatisticCard from "@/components/statistic-card-2.tsx";
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-50 p-6">
+      <StatisticCard />
+    </div>
+  )
+}
